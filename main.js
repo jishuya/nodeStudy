9f7e8f18e36efede906b7714f019cbf4a543ba81
jishu <jishuya3015@gmail.com>
2021-12-24 09:49:23 +0900

    Asynchronous test

--- main.js
+++ main.js
@@ -1,12 +1,22 @@
-const Event = require('events');
-const emitter = new Event();
+const fs = require('fs');
 
-// on 메소드: 특정 이벤트가 발생했을 때, 실행 할 콜백 등록하기 
-// 코어모듈에서 가져다 쓸 다양한 함수들이 이벤트 기반으로 동작하도록 설계 되었다.
-// 코어모듈의 많은 객체들이 EventEmitter 객체이다
+console.log("start")
+fs.readFile('test.js', 'utf8', (err, content)=>{
+  console.log("content: ", content)
+})
+console.log("finish")
 
-emitter.on('test', () =>{ 
-  console.log('Success!');
-});
 
-emitter.emit('test');
+
+// const Event = require('events');
+// const emitter = new Event();
+
+// // on 메소드: 특정 이벤트가 발생했을 때, 실행 할 콜백 등록하기 
+// // 코어모듈에서 가져다 쓸 다양한 함수들이 이벤트 기반으로 동작하도록 설계 되었다.
+// // 코어모듈의 많은 객체들이 EventEmitter 객체이다
+
+// emitter.on('test', () =>{ 
+//   console.log('Success!');
+// });
+
+// emitter.emit('test');
