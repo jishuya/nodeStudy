00293d63af98ac515bf2c905c8d213c9fd2d89fe
jishu <jishuya3015@gmail.com>
2021-12-25 07:52:36 +0900

    Emitter

--- main.js
+++ main.js
@@ -1,11 +1,53 @@
+const EventEmitter = require('events');
+const myEmitter = new EventEmitter();
+// const myEmitter2 = new EventEmitter();
+const obj = { '이름': '지금', '나이': 20, '수강여부': true  }
 
-const fs = require('fs');
+myEmitter.on('test', (info)=>{
+ console.log(info)
+})
 
-let fileList = fs.readdirSync('.');
-console.log(fileList)
+myEmitter.emit('test', obj)
 
-let newFile = fs.writeFileSync('newFile', 'Hello! nodejs!')
 
-const os = require('os');
-console.log(os.cpus())
 
+myEmitter.once('test', ()=>{
+  console.log('hahaha')
+})
+
+
+console.log(myEmitter.listeners('test'))
+
+
+const arr =[]
+
+arr[0] = () => {
+  console.log("callback0")
+}
+
+arr[1] = () => {
+  console.log("callback1")
+}
+
+myEmitter.on('test', arr[0])
+myEmitter.on('test', arr[1])
+
+myEmitter.off('test', arr[0])
+
+myEmitter.emit('test')
+
+
+myEmitter.on('test', offTest)
+myEmitter.off('test', offTest)
+
+myEmitter.on('test', ()=>{
+  console.log("test2")
+})
+
+myEmitter.on('test', ()=>{
+  console.log("test3")
+})
+
+myEmitter2.on('test', ()=>{
+  console.log("test4")
+})
