94d4bbeee6732e40b144f165da84148b7e072a1b
jishu <jishuya3015@gmail.com>
2021-12-27 07:12:22 +0900

    create server

--- main.js
+++ main.js
@@ -1,53 +1,60 @@
-const EventEmitter = require('events');
-const myEmitter = new EventEmitter();
-// const myEmitter2 = new EventEmitter();
-const obj = { '이름': '지금', '나이': 20, '수강여부': true  }
-
-myEmitter.on('test', (info)=>{
- console.log(info)
+const http = require('http')
+const server = http.createServer(function(request, response){
+  response.end('<h1>HELLO WORLD!</h1>')
 })
 
-myEmitter.emit('test', obj)
+server.listen(3000);
+
+// const EventEmitter = require('events');
+// const myEmitter = new EventEmitter();
+// // const myEmitter2 = new EventEmitter();
+// const obj = { '이름': '지금', '나이': 20, '수강여부': true  }
+
+// myEmitter.on('test', (info)=>{
+//  console.log(info)
+// })
+
+// myEmitter.emit('test', obj)
 
 
 
-myEmitter.once('test', ()=>{
-  console.log('hahaha')
-})
+// myEmitter.once('test', ()=>{
+//   console.log('hahaha')
+// })
 
 
-console.log(myEmitter.listeners('test'))
+// console.log(myEmitter.listeners('test'))
 
 
-const arr =[]
+// const arr =[]
 
-arr[0] = () => {
-  console.log("callback0")
-}
+// arr[0] = () => {
+//   console.log("callback0")
+// }
 
-arr[1] = () => {
-  console.log("callback1")
-}
+// arr[1] = () => {
+//   console.log("callback1")
+// }
 
-myEmitter.on('test', arr[0])
-myEmitter.on('test', arr[1])
+// myEmitter.on('test', arr[0])
+// myEmitter.on('test', arr[1])
 
-myEmitter.off('test', arr[0])
+// myEmitter.off('test', arr[0])
 
-myEmitter.emit('test')
+// myEmitter.emit('test')
 
 
-myEmitter.on('test', offTest)
-myEmitter.off('test', offTest)
+// myEmitter.on('test', offTest)
+// myEmitter.off('test', offTest)
 
-myEmitter.on('test', ()=>{
-  console.log("test2")
-})
+// myEmitter.on('test', ()=>{
+//   console.log("test2")
+// })
 
-myEmitter.on('test', ()=>{
-  console.log("test3")
-})
+// myEmitter.on('test', ()=>{
+//   console.log("test3")
+// })
 
-myEmitter2.on('test', ()=>{
-  console.log("test4")
-})
+// myEmitter2.on('test', ()=>{
+//   console.log("test4")
+// })
