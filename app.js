23c1c74992827f6d66e82769c877e916c62d94ab
jishu <jishuya3015@gmail.com>
2022-01-12 15:58:05 +0900

    node error reset

--- app.js
+++ app.js
@@ -1,46 +1,39 @@
-const express = require('express')
-const app = express();
+const express = require('express');
 const members = require('./members');
+
+const app = express();
+
 app.use(express.json());
-
-app.use((req, res, next) => {
-    console.log("middleware test....")
-    console.log(req.query)
-    next();
-})
-
-app.get('/', (req, res) => {
-    res.send('<h1>start page.... </h1>')
-}); 
+// 여기에 두 번째 미들웨어를 설정해보세요.
 
 
-app.post('/api/members', (req, res) => {
-    const newMember = req.body;
-    members.push(newMember)
-    res.send(newMember)
+
+app.get('/api/members', (req, res) => {
+  const { team } = req.query;
+  if (team) {
+    const teamMembers = members.filter((m) => m.team === team);
+    res.send(teamMembers);
+  } else {
+    res.send(members);
+  }
 });
 
 app.get('/api/members/:id', (req, res) => {
-    const { id } = req.params;
-    const member = members.find((m) => { return m.id === Number(id)});
+  const { id } = req.params;
+  const member = members.find((m) => m.id === Number(id));
+  if (member) {
+    res.send(member);
+  } else {
+    res.status(404).send({ message: 'There is no such member' });
+  }
+});
 
-    if(member) {
-        res.send(member)
-    } else {
-        res.status(404).send({ message: 'There is no such member...' });
-    }
-})
+app.post('/api/members', (req, res) => {
+  const newMember = req.body;
+  members.push(newMember);
+  res.send(newMember);
+});
 
-// app.get('/api/members/', (req, res) => {
-//     const { team } = req.query;
-//     if (team) {
-//         const teamMember = members.filter((m) => { return m.team === team});
-//         res.send(teamMember);
-//     } else {
-//         res.send(members)
-//     }
-// })
-
-app.listen(3000, () =>{
-    console.log('Server is listening')
-})
+app.listen(3000, () => {
+  console.log('Server is listening...');
+});
