c36c3f492db86477e09e414269dec134c5c7c4e2
jishu <jishuya3015@gmail.com>
2022-02-07 09:58:25 +0900

    url component

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
-const EventEmitter = require('events');
-const emitterTest = new EventEmitter();
-
-emitterTest.on('test', () => {
-    console.log('Emitter is working...')
-});
-
-emitterTest.emit('test');
+let url = new URL('http://nodestudy.com/coding/nodejs/capter?page=5&line=23');
+console.log(url.protocol);
+console.log(url.host);
+console.log(url.pathname);
+console.log(url.search);
