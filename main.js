199a6b386818a2acae80e6010db5fb8030f159d7
jishu <jishuya3015@gmail.com>
2022-02-24 09:30:01 +0900

    routing test review

--- main.js
+++ main.js
@@ -1,5 +1,39 @@
-let url = new URL('http://nodestudy.com/coding/nodejs/capter?page=5&line=23');
-console.log(url.protocol);
-console.log(url.host);
-console.log(url.pathname);
-console.log(url.search);
+// const menu = ['Pizza', 'Pasta', 'Steak', 'Salad', 'Soup']
+
+// const http = require('http');
+// const server = http.createServer((req, res) => {
+//     if (req.url === '/') {
+//         res.end(`<h1>Welcome our restaurant!</h1>`);
+//     } else if (req.url === '/menu') {
+//         res.end(  `<h1>${menu}</h1>`)
+//     } else {
+//         res.end(`<h1>There is an error</h1>`)
+//     }
+// })
+
+
+// server.listen(3000);
+
+const menu = ['Pizza', 'Pasta', 'Steak', 'Salad', 'Soup']
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res)=>{
+    res.end(`<h1>Welcome our restaurant!</h1>`)
+})
+
+app.get('/menu', (req, res)=>{
+    res.end(`<h1>${menu}</h1>`)
+})
+
+app.get('/menu/:food', (req, res)=>{
+    const foodName = menu[req.params.food -1];
+    console.log(11111, foodName)
+    res.end(`<h1>${foodName}</h1>`)
+})
+
+app.get('*', (req, res)=>{
+    res.end(`<h1>The page cannot be found.</h1>`)
+})
+
+app.listen(5000);
